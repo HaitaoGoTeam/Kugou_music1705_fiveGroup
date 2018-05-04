1e818b624b75d70b451ec09a9022965fb6f836db
haitao <m18533176829@163.com>
2018-05-04 13:28:58 +0800

    haitaoGo 1.0框架 task analysislog ...1.0

---
+++ src/main/scala/daoke360com/task/analysislog/AnalysisLogTask.scala
@@ -0,0 +1,22 @@
+package daoke360com.task.analysislog
+
+/**
+  * Created by dell on 2018/5/4.
+  */
+object AnalysisLogTask {
+
+
+  def main(args: Array[String]): Unit = {
+
+
+
+
+
+
+
+
+
+  }
+
+
+}
---
+++ src/main/scala/daoke360com/task/analysislog/log_utils/LogAnalysisUtils.scala
@@ -0,0 +1,8 @@
+package daoke360com.task.analysislog.log_utils
+
+/**
+  * Created by dell on 2018/5/4.
+  */
+object LogAnalysisUtils {
+
+}
